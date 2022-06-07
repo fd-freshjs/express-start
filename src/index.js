52c0506bd9135166c3e2441ba3d5c8a019d7cebb
Yevhenii Lapshynov <fd-freshjs>
2022-06-07 17:06:09 +0300

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-// common js modules
 const express = require('express');
-const { clientErrorsHandler, serverErrorHandler } = require('./middlewares/error.handler.js');
+const path = require('path');
+const {
+  clientErrorsHandler,
+  serverErrorHandler,
+} = require('./middlewares/error.handler.js');
 const router = require('./routes/index.js');
+const dotenv = require('dotenv');
+dotenv.config(path.resolve(__dirname, '../', '.env'));
 
-// Create a server to receive data from clients
 const app = express();
 
-app.use(express.json()); // Content-Type: application/json
+app.use(express.json());
+
+app.use('/files', express.static(
+  path.resolve(__dirname, '../', 'public')
+)); // public/avatars -> /static/avatars
 
 // cors
 
 app.use(function (req, res, next) {
-    console.log(new Date().toLocaleString(), req.method, req.path, req.body);
-    next();
+  console.log(new Date().toLocaleString(), req.method, req.path, req.body);
+  next();
 });
 
 app.use('/', router);
@@ -20,7 +28,8 @@ app.use('/', router);
 app.use(clientErrorsHandler);
 app.use(serverErrorHandler);
 
+const port = process.env.PORT || 5000;
 // http://localhost:5000
-app.listen(5000, () => {
-    console.log('Server started');
+app.listen(port, () => {
+  console.log('Server started on port', port);
 });
